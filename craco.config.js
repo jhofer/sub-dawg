c75dbbd801986bc78ca721b65fc941fea68593e1
Jonas Hofer <jonas.hofer@outlook.com>
2021-11-20 11:46:46 +0100

    output to docs

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,20 @@
-const CracoLessPlugin = require('craco-less');
+const CracoLessPlugin = require("craco-less");
+const path = require('path');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = webpackConfig.output.path = path.resolve("docs");
+      return webpackConfig; 
+    },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
+            modifyVars: { "@primary-color": "#1DA57A" },
             javascriptEnabled: true,
           },
         },
